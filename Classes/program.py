71cb966ea649c0e0b1f31ec14b77a6cc85121362
Zumar Syed Bukhari <164260201+Zumar15@users.noreply.github.com>
2026-04-22 15:27:45 -0600

    Create program.py
    This is a starting point for the program.

---
+++ Classes/program.py
@@ -0,0 +1,4 @@
+print("Welcome to the Intelligent Transit Route Optimizer!")
+#This message will display what route is being used.
+print("The program will display route information for the route... ")
+#The classes will placed here to run the entire program.
